be4dfa0a836469b10bca8267670ca028b569c7cb
Vdmrl <vvluzin2004@gmail.com>
2024-04-01 00:45:48 +0500

    fix: accounts_to_texts method now get all users of project and their roles in project

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from services.data_to_texts import UserData, SupportData
 
 async def main():
     user_data = UserData()
-    await user_data.account_to_texts(2)
+    await user_data.accounts_to_texts(41)
     await user_data.event_to_texts(41)
     print(user_data.texts)
 
--- backend/services/data_to_texts.py
+++ backend/services/data_to_texts.py
@@ -2,7 +2,7 @@ from sqlalchemy import select
 from sqlalchemy.orm import joinedload, selectinload
 
 from db.engine import async_session_factory
-from db.models.startup import Users  # account
+from db.models.startup import Users, UserProject  # account
 from db.models.startup import Projects, ProjectData, Forms, FormFields  # project data
 from db.models.startup import Passports, PassportData, PassportFields  # project passports
 
@@ -18,45 +18,60 @@ class UserData:
     def __init__(self):
         self.texts = []
 
-    async def account_to_texts(self, id_user: int) -> None:
+    async def accounts_to_texts(self, id_project: int) -> None:
         """
         denormalizes account data into text
         :rtype: user id pk
         """
-        async with async_session_factory() as session:
-            query = select(Users).where(Users.id == id_user)
+        async with (async_session_factory() as session):
+            query = (select(Projects)
+                     .options(  # forms
+                selectinload(Projects.user_project)  # one-to-many
+                .joinedload(UserProject.users))  # many-to-one
+                     .where(Projects.id_projects == id_project)
+                     )
             result = await session.execute(query)
-            user = result.scalar_one_or_none()
-            if user:
-                self.texts.append(user.country)
-                self.texts.append(user.country)
-                self.texts.append(user.region)
-                self.texts.append(user.locality)
-                self.texts.append(user.place_of_work_study)
-                self.texts.append(user.name_of_company)
-                self.texts.append(user.position)
-                self.texts.append(user.education)
-                self.texts.append(user.specialty)
-                self.texts.append(user.university)
-                self.texts.append(user.academic_degree)
-                self.texts.append(user.academic_rank)
-                self.texts.append(user.experience)
-                self.texts.append(user.progress)
-                self.texts.append(user.resume1)
-                self.texts.append(user.resume2)
-                self.texts.append(user.resume3)
-                self.texts.append(user.year_of_graduation)
-                self.texts.append(user.scientific_specialty)
-                self.texts.append(user.work_experience)
-                self.texts.append(user.tracking_experience)
-                self.texts.append(user.business_experience)
-                self.texts.append(user.teaching_experience)
-                self.texts.append(user.expertise_experience)
-                self.texts.append(user.main_publications)
-                self.texts.append(user.languages)
-                self.texts.append(user.programs)
+            project = result.scalar_one_or_none()
+            if project:
+                user_projects = project.user_project
+                if user_projects:
+                    for user_project in user_projects:
+                        self.texts.append(user_project.role_description)
+                        user = user_project.users
+                        if user:
+                            self.texts.append(user.country)
+                            self.texts.append(user.country)
+                            self.texts.append(user.region)
+                            self.texts.append(user.locality)
+                            self.texts.append(user.place_of_work_study)
+                            self.texts.append(user.name_of_company)
+                            self.texts.append(user.position)
+                            self.texts.append(user.education)
+                            self.texts.append(user.specialty)
+                            self.texts.append(user.university)
+                            self.texts.append(user.academic_degree)
+                            self.texts.append(user.academic_rank)
+                            self.texts.append(user.experience)
+                            self.texts.append(user.progress)
+                            self.texts.append(user.resume1)
+                            self.texts.append(user.resume2)
+                            self.texts.append(user.resume3)
+                            self.texts.append(user.year_of_graduation)
+                            self.texts.append(user.scientific_specialty)
+                            self.texts.append(user.work_experience)
+                            self.texts.append(user.tracking_experience)
+                            self.texts.append(user.business_experience)
+                            self.texts.append(user.teaching_experience)
+                            self.texts.append(user.expertise_experience)
+                            self.texts.append(user.main_publications)
+                            self.texts.append(user.languages)
+                            self.texts.append(user.programs)
+                        else:
+                            logging.warning("no user in data_to_text")
+                else:
+                    logging.warning("no user_project in data_to_text")
             else:
-                logging.warning("no user in data_to_text")
+                logging.warning("no project in data_to_text")
 
     async def project_data_to_texts(self, id_project: int) -> None:
         """
@@ -180,6 +195,7 @@ class SupportData:
                         if region:
                             self.texts.append(region.region_name)
                             self.texts.append(region.region_code)
+
             else:
                 logging.warning("no support in data_to_text")
 
